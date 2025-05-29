43e0c5a36979238da803fa177a47647583420abc
hoanghuytoi <hoanghuytoi03@Gmail.com>
2025-05-29 20:12:46 +0700

    update

---
+++ BackEnd_Shop_Phu_Kien/shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
@@ -0,0 +1,95 @@
+package com.project.shopapp.controllers;
+import java.util.List;
+
+import com.project.shopapp.dtos.requests.user.UpdateUserDTO;
+import com.project.shopapp.dtos.requests.user.UserDTO;
+import com.project.shopapp.dtos.requests.user.UserLoginDTO;
+import com.project.shopapp.dtos.responses.user.LoginResponse;
+import com.project.shopapp.dtos.responses.user.RegisterResponse;
+import com.project.shopapp.dtos.responses.user.UserResponse;
+import com.project.shopapp.models.User;
+import com.project.shopapp.services.user.IUserService;
+import com.project.shopapp.components.LocalizationUtils;
+import com.project.shopapp.utils.MessageKeys;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping("${api.prefix}/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final IUserService userService;
+    private final LocalizationUtils localizationUtils;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponse> createUser(@Valid @RequestBody UserDTO userDTO, BindingResult bindingResult){
+        try {
+            if (bindingResult.hasErrors()) {
+                List<String> errorMessages = bindingResult.getFieldErrors().stream().map(FieldError::getDefaultMessage).toList();
+                return ResponseEntity.badRequest().body(
+                        RegisterResponse.builder().message(localizationUtils.getLocalizedMessage(MessageKeys.REGISTER_FAILED, errorMessages)).build()
+                );
+            }
+            if(!userDTO.getPassWord().equals(userDTO.getRetypePassword())){
+                return ResponseEntity.badRequest().body(
+                        RegisterResponse.builder().message(localizationUtils.getLocalizedMessage(localizationUtils.getLocalizedMessage(MessageKeys.REGISTER_FAILED))).build()
+                );
+            }
+            User user = userService.createUser(userDTO);
+            return ResponseEntity.ok(RegisterResponse.builder().message(localizationUtils.getLocalizedMessage(MessageKeys.REGISTER_SUCCESSFULLY)).user(user).build());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(RegisterResponse.builder().message(localizationUtils.getLocalizedMessage(MessageKeys.REGISTER_FAILED, e.getMessage())).build());
+        }
+    }
+
+        @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginUser(@Valid @RequestBody UserLoginDTO userLoginDTO){
+        try {
+            String token = userService.login(userLoginDTO.getPhoneNumber(), userLoginDTO.getPassWord(),  userLoginDTO.getRoleId() == null ? 1 : userLoginDTO.getRoleId());
+
+            return ResponseEntity.ok(LoginResponse.builder().message(localizationUtils.getLocalizedMessage(MessageKeys.LOGIN_SUCCESSFULLY)).token(token).build());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(LoginResponse.builder().message(localizationUtils.getLocalizedMessage(MessageKeys.LOGIN_FAILED, e.getMessage())).build());
+        }
+    }
+
+    @PostMapping("/details")
+    public ResponseEntity<UserResponse> getUserDetails(@RequestHeader("Authorization") String authorizationHeader) {
+        try {
+            String extractedToken = authorizationHeader.substring(7); // Loại bỏ "Bearer " từ chuỗi token
+            User user = userService.getUserDetailsFromToken(extractedToken);
+            return ResponseEntity.ok(UserResponse.fromUser(user));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/details/{userId}")
+    public ResponseEntity<UserResponse> updateUserDetails(
+            @PathVariable int userId,
+            @RequestBody UpdateUserDTO updatedUserDTO,
+            @RequestHeader("Authorization") String authorizationHeader
+    ) {
+        try {
+            String extractedToken = authorizationHeader.substring(7);
+            User user = userService.getUserDetailsFromToken(extractedToken);
+            // Ensure that the user making the request matches the user being updated
+            if (user.getId() != userId) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            }
+            User updatedUser = userService.updateUser(userId, updatedUserDTO);
+            return ResponseEntity.ok(UserResponse.fromUser(updatedUser));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+    
+}
